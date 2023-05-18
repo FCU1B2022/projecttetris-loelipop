b893e6a42cc254bb81c17d62e1d3a8d4c8a72c8f
loelipop <loejunxiang@gmail.com>
2023-05-18 11:00:09 +0800

    week3

--- ProjectTetris/main.c
+++ ProjectTetris/main.c
@@ -2,37 +2,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <windows.h>
 
 #define CANVAS_WIDTH 10
 #define CANVAS_HEIGHT 20
 
 typedef enum {
-	RED = 41,
-	GREEN,
-	YELLOW,
-	BLUE,
-	PURPLE,
-	CYAN,
-	WHITE,
-	BLACK = 0,
-}Color ;
+    RED = 41,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE,
+    BLACK = 0,
+}Color;
 
 typedef enum {
-	EMPTY = -1,
-	I,
-	J,
-	L,
-	O,
-	S,
-	T,
-	Z
+    EMPTY = -1,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
 }ShapeId;
 
 typedef struct {
-	ShapeId shape;
-	Color color;
-	int size;
-	char rotates[4][4][4];
+    ShapeId shape;
+    Color color;
+    int size;
+    char rotates[4][4][4];
 }Shape;
 
 typedef struct
@@ -45,7 +46,7 @@ typedef struct
     ShapeId queue[4];
 }State;
 
-typedef struct{
+typedef struct {
     Color color;
     ShapeId shape;
     bool current;
@@ -250,11 +251,77 @@ Shape shapes[7] = {
     },
 };
 
-void resetBlock(Block *block)
+void setBlock(Block* block, Color color, ShapeId shape, bool current)
+{
+    block->color = color;
+    block->shape = shape;
+    block->current = current;
+}
+
+void resetBlock(Block* block)
 {
     block->color = BLACK;
     block->shape = EMPTY;
     block->current = false;
+}
+
+bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int originalY, int originalRotate, int newX, int newY, int newRotate, ShapeId shapeId) {
+    Shape shapeData = shapes[shapeId];
+    int size = shapeData.size;
+
+    // check if the new position is valid to place the block
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (shapeData.rotates[newRotate][i][j]) {
+                if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i < 0 || newY + i >= CANVAS_HEIGHT) {
+                    return false;
+                }
+                if (!canvas[newY + i][newX + j].current && canvas[newY + i][newX + j].shape != EMPTY) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    // remove the old position
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (shapeData.rotates[originalRotate][i][j]) {
+                resetBlock(&canvas[originalY + i][originalX + j]);
+            }
+        }
+    }
+
+    // move the block
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (shapeData.rotates[newRotate][i][j]) {
+                setBlock(&canvas[newY + i][newX + j], shapeData.color, shapeId, true);
+            }
+        }
+    }
+
+    return true;
+}
+
+void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
+{
+    printf("\033[0;0H\n");
+    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+        printf("|");
+        for (int j = 0; j < CANVAS_WIDTH; j++) {
+            printf("\033[%dm\u3000", canvas[i][j].color);
+        }
+        printf("\033[0m|\n");
+    }
+    return;
+}
+
+void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
+{
+    if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
+        state->y++;
+    return;
 }
 
 int main()
@@ -268,15 +335,15 @@ int main()
         .fallTime = 0
     };
 
-    for(int i = 0;i < 4;i++)
+    for (int i = 0; i < 5; i++)
     {
         state.queue[i] = rand() % 7;
     }
 
     Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
-    for(int i = 0;i < CANVAS_HEIGHT;i++)
+    for (int i = 0; i < CANVAS_HEIGHT; i++)
     {
-        for(int j = 0;j < CANVAS_WIDTH;j++)
+        for (int j = 0; j < CANVAS_WIDTH; j++)
         {
             resetBlock(&canvas[i][j]);
         }
@@ -284,5 +351,14 @@ int main()
 
     system("cls");
     printf("\e[?25l"); // hide cursor
+
+    move(canvas, state.x, state.y, state.rotate, state.x, state.y, state.rotate, state.queue[0]);
+
+    while (1)
+    {
+        logic(canvas, &state);
+        printCanvas(canvas, &state);
+        Sleep(100);
+    }
 
 }
